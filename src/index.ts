e8d88ad61d34d515249d9eb56ab8268bf1888f2e
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-02-27 12:58:13 +0200

    feat: add src folder

---
+++ src/index.ts
@@ -0,0 +1,76 @@
+function isPlatform(searchTerm: string | RegExp): boolean {
+  const platform = navigator.platform;
+  if (typeof searchTerm === "string") {
+    return platform.includes(searchTerm);
+  } else {
+    return searchTerm.test(platform);
+  }
+}
+
+function getCurrentOs(): "Mac" | "Linux" | "Windows" | "Unknown" {
+  if (isPlatform("Mac")) {
+    return "Mac";
+  }
+  if (isPlatform("Linux")) {
+    return "Linux";
+  }
+  if (isPlatform("Windows")) {
+    return "Windows";
+  }
+  return "Unknown";
+}
+
+let capsState = false;
+const os = getCurrentOs();
+let onCapsChangeHandler: (capsState: boolean) => void;
+
+function getCapsLockModifierState(event: KeyboardEvent): boolean {
+  return event.getModifierState("CapsLock");
+}
+
+document.addEventListener("keyup", (event) => {
+  if (os === "Mac") {
+    // macOS sends only keydown when enabling Caps Lock and only keyup when disabling.
+    if (event.key === "CapsLock") {
+      capsState = false;
+    } else {
+      // IPad doesn't send caps state on any keypress which isn't Caps Lock,
+      // So don't update caps state on any keypress which isn't Caps Lock.
+      if (navigator.maxTouchPoints <= 1) {
+        capsState = getCapsLockModifierState(event);
+      }
+    }
+  } else if (os === "Windows") {
+    // Windows always sends the correct state on keyup (for Caps Lock and for regular keys).
+    capsState = getCapsLockModifierState(event);
+  } else if (event.key !== "CapsLock") {
+    // Linux sends the correct state on keyup if key isn't Caps Lock.
+    capsState = getCapsLockModifierState(event);
+  }
+  onCapsChangeHandler(capsState);
+});
+
+document.addEventListener("keydown", (event) => {
+  if (os === "Mac") {
+    // macOS sends only keydown when enabling Caps Lock and only keyup when disabling.
+    if (event.key === "CapsLock") {
+      capsState = true;
+      onCapsChangeHandler(capsState);
+    }
+  } else if (os === "Linux") {
+    /* Linux sends the correct state before Caps Lock is toggled only on keydown,
+     * so we invert the modifier state.
+     */
+    if (event.key === "CapsLock") {
+      capsState = !getCapsLockModifierState(event);
+    }
+  }
+});
+
+export function isCapsLockOn(): boolean {
+  return capsState;
+} 
+
+export function onCapsLockChange(handler: (capsState: boolean) => void): void {
+  onCapsChangeHandler = handler;
+}
