de5366061c654a7d3f52c932a133f2e71e2184ff
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2026-07-15 19:50:35 +0300

    refactor: use switch statements

--- src/index.ts
+++ src/index.ts
@@ -66,34 +66,41 @@ document.addEventListener("keyup", (event) => {
     afterKeyup.delete(event.code);
     return;
   }
-  if (os === "Mac") {
-    // macOS sends only keydown when enabling Caps Lock and only keyup when disabling.
-    if (event.key === "CapsLock") {
-      setCapsState(false);
-      return;
-    }
-    /*
-     * The iPad's default virtual keyboard doesn't send Caps Lock state on any keypress which isn't Caps Lock,
-     * So to decide whether to ignore Caps Lock state on other keypresses,
-     * We check whether getCapsLockModifierState has ever returned true.
-     * When Caps Lock is pressed, handle it the same as on macOS.
-     */
-    const currentCapsState = getCapsLockModifierState(event);
-    if (isSendingCapsLockState || currentCapsState) {
-      // macOS sends correct state on keyup.
-      setCapsState(currentCapsState);
-      isSendingCapsLockState = true;
-    }
-  } else if (os === "Windows") {
-    // Windows always sends the correct Caps Lock state on keyup (for Caps Lock and for regular keys).
-    setCapsState(getCapsLockModifierState(event));
-  } else if (os === "Linux") {
-    if (event.key !== "CapsLock" && event.key !== "Unidentified") {
-      // Check whether key is Unidentified because GBoard sends Unidentified keypresses
-      // Which don't have Caps State.
-      // Linux on Wayland and Linux with Chromium on X11/Xwayland send the correct Caps Lock state on keyup if the key isn't Caps Lock.
+
+  switch (os) {
+    case "Windows":
+      // Windows always sends the correct Caps Lock state on keyup (for Caps Lock and for regular keys).
       setCapsState(getCapsLockModifierState(event));
-    }
+      break;
+    case "Mac":
+      // macOS sends only keydown when enabling Caps Lock and only keyup when disabling.
+      if (event.key === "CapsLock") {
+        setCapsState(false);
+        return;
+      }
+      /*
+       * The iPad's default virtual keyboard doesn't send Caps Lock state on any keypress which isn't Caps Lock,
+       * So to decide whether to ignore Caps Lock state on other keypresses,
+       * We check whether getCapsLockModifierState has ever returned true.
+       * When Caps Lock is pressed, handle it the same as on macOS.
+       */
+      {
+        const currentCapsState = getCapsLockModifierState(event);
+        if (isSendingCapsLockState || currentCapsState) {
+          // macOS sends correct state on keyup.
+          setCapsState(currentCapsState);
+          isSendingCapsLockState = true;
+        }
+      }
+      break;
+    case "Linux":
+      if (event.key !== "CapsLock" && event.key !== "Unidentified") {
+        // Check whether key is Unidentified because GBoard sends Unidentified keypresses
+        // Which don't have Caps State.
+        // Linux on Wayland and Linux with Chromium on X11/Xwayland send the correct Caps Lock state on keyup if the key isn't Caps Lock.
+        setCapsState(getCapsLockModifierState(event));
+      }
+      break;
   }
 });
 
@@ -102,52 +109,58 @@ document.addEventListener("keydown", (event) => {
     afterKeyup.delete(event.code);
   }
 
-  if (os === "Windows") {
-    // Windows always sends the correct Caps Lock state on keydown (for Caps Lock and for regular keys).
-    setCapsState(getCapsLockModifierState(event));
-  } else if (os === "Mac") {
-    // macOS sends only keydown when enabling Caps Lock and only keyup when disabling.
-    if (event.key === "CapsLock") {
-      setCapsState(true);
-    }
-  } else if (os === "Linux") {
-    /*
-     * Linux on Wayland sends the correct Caps Lock state before toggling Caps Lock
-     * on keydown, so we invert the Caps Lock state to get the state after the toggle.
-     * On keyup, Linux on Wayland always sends `true` for Caps Lock state when toggling
-     * Caps Lock.
-     *
-     * Linux with Firefox on X11/Xwayland sends the correct Caps Lock state for all keys
-     * on keyup and always sends `true` for Caps Lock state on keydown when toggling
-     * Caps Lock. Unfortunately, we can't differentiate between Wayland and X11/Xwayland,
-     * so we currently only support Wayland.
-     *
-     * Linux with Chromium on X11/Xwayland has the same Caps Lock behavior as Linux on
-     * Wayland, so it's also supported.
-     */
-    if (event.key === "CapsLock") {
-      const flippedCapsState = !getCapsLockModifierState(event);
+  switch (os) {
+    case "Windows":
+      // Windows always sends the correct Caps Lock state on keydown (for Caps Lock and for regular keys).
+      setCapsState(getCapsLockModifierState(event));
 
-      if (flippedCapsState) {
+      break;
+    case "Mac":
+      // macOS sends only keydown when enabling Caps Lock and only keyup when disabling.
+      if (event.key === "CapsLock") {
         setCapsState(true);
-      } else {
-        /* Defer change to be ran when Caps Lock is disabled. This is to prevent the
-         * following case from returning an incorrect value:
-         *
-         * 1. Enable Caps Lock
-         * 2. Press and hold Caps Lock
-         * 3. Press another key and release it
-         * 4. Release Caps Lock
-         *
-         * Without deferring, this would cause `capsState` to stay `true`, because the
-         * change made here would be overridden by the change to the Caps Lock state when
-         * the other key releases. Deferring the change makes it run after the other key
-         * releases and changes the Caps Lock state.
-         */
-
-        afterKeyup.set(event.code, flippedCapsState);
       }
-    }
+
+      break;
+    case "Linux":
+      /*
+       * Linux on Wayland sends the correct Caps Lock state before toggling Caps Lock
+       * on keydown, so we invert the Caps Lock state to get the state after the toggle.
+       * On keyup, Linux on Wayland always sends `true` for Caps Lock state when toggling
+       * Caps Lock.
+       *
+       * Linux with Firefox on X11/Xwayland sends the correct Caps Lock state for all keys
+       * on keyup and always sends `true` for Caps Lock state on keydown when toggling
+       * Caps Lock. Unfortunately, we can't differentiate between Wayland and X11/Xwayland,
+       * so we currently only support Wayland.
+       *
+       * Linux with Chromium on X11/Xwayland has the same Caps Lock behavior as Linux on
+       * Wayland, so it's also supported.
+       */
+      if (event.key === "CapsLock") {
+        const flippedCapsState = !getCapsLockModifierState(event);
+
+        if (flippedCapsState) {
+          setCapsState(true);
+        } else {
+          /* Defer change to be ran when Caps Lock is disabled. This is to prevent the
+           * following case from returning an incorrect value:
+           *
+           * 1. Enable Caps Lock
+           * 2. Press and hold Caps Lock
+           * 3. Press another key and release it
+           * 4. Release Caps Lock
+           *
+           * Without deferring, this would cause `capsState` to stay `true`, because the
+           * change made here would be overridden by the change to the Caps Lock state when
+           * the other key releases. Deferring the change makes it run after the other key
+           * releases and changes the Caps Lock state.
+           */
+
+          afterKeyup.set(event.code, flippedCapsState);
+        }
+      }
+      break;
   }
 });
 
